102bf45c78cb9f08902d64b2e01f133d5920727f
Hussain-7 <hussain2000.rizvi@gmail.com>
2026-03-07 00:10:38 +0000

    remove extra code

--- src/lib/inngest.ts
+++
@@ -1,8 +0,0 @@
-import { Inngest } from "inngest";
-
-const eventKey = process.env.INNGEST_EVENT_KEY;
-
-export const inngest = new Inngest({
-  id: "endless-dev",
-  ...(eventKey ? { eventKey } : {}),
-});
